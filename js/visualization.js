02298cc8ee3bf41295c001dbe9bb7f4ae8eb9bc0
jdflodman <57110574+jdflodman@users.noreply.github.com>
2021-11-09 12:37:14 -0500

    setup

--- js/visualization.js
+++ js/visualization.js
@@ -1,16 +1,43 @@
-// Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
-((() => {
-
-  console.log('Hello, world!');
-
-})());
-
-
-d3.csv('data/small_data.csv').then((data) => {
+// read in data as example for PM06
+d3.csv('data/all_vis.csv').then((data) => {
 
 	//print data to log
-	console.log(data);
+	console.log(data.slice(0,10));
 
 
 })
+
+
+// SPACE FOR VIS 1: BAR CHART AND SCATTER PLOT
+
+
+
+
+
+
+
+// END OF SPACE FOR VIS 1
+
+
+
+// SPACE FOR VIS 2: 2 BAR CHARTS
+
+
+
+
+
+
+// END OF SPACE FOR VIS 2
+
+
+
+
+
+
+// SPACE FOR VIS 3: 3 CHARTS
+
+
+
+
+
+// END OF SPACE FOR VIS 3
