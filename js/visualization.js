26d7326f9fcb40c48c218340407088c92332a1b5
jdflodman <57110574+jdflodman@users.noreply.github.com>
2021-11-09 13:19:31 -0500

    push

--- js/visualization.js
+++ js/visualization.js
@@ -7,12 +7,59 @@ d3.csv('data/all_vis.csv').then((data) => {
 
 })
 
+// Variables
+var margin = { top: 10, right: 30, bottom: 50, left: 60 },
+  width = 460 - margin.left - margin.right,
+  height = 450 - margin.top - margin.bottom;
 
-// SPACE FOR VIS 1: BAR CHART AND SCATTER PLOT
+
+// SPACE FOR VIS 1: BAR CHART AND SCATTER PLOT (JUSTIN)
+
+var svg1 = d3
+  .select("#vis1")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
+d3.csv('data/all_vis.csv', function(row) {
+
+	// get weekday for each entry
+	const parseTime = d3.timeParse('%Y-%m-%d %H:%M:%S');
+	const formatTime = d3.timeFormat('%a');
+	const formatHours = d3.timeFormat('%X');
+	d3Time = parseTime(row.datetime)
+	Weekday = formatTime(d3Time)
+	Time = formatHours(d3Time)
+	
+	// format the data
+	return {
+		Weekday: Weekday, 
+		SurgeMultiplier: +row.surge_multiplier,
+		Price: +row.price,
+		Time: Time,
+		CabType : row.cab_type
+	}
 
 
+	}).then((data) => {
+
+		// bar chart
+		// pseudo-code:
+		// -- hard code each of the days of the week's values
+		
+
+
+		// scatter plot
+		// pseudo-code: 
+		// - on hover, 
+		// - clear the existing scatter plot
+		// - trigger the drawing of a scatter plot
+
+
+})
 
 
 
